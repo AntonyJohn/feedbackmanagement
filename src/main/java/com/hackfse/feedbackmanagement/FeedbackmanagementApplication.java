7019aa2f0ebb5f85a29c2bba9b773368d746d77c
AntonyJohn <m.antonyjohn@gmail.com>
2019-02-11 00:51:29 +0530

    JPA configuration

--- src/main/java/com/hackfse/feedbackmanagement/FeedbackmanagementApplication.java
+++ src/main/java/com/hackfse/feedbackmanagement/FeedbackmanagementApplication.java
@@ -1,14 +1,28 @@
 package com.hackfse.feedbackmanagement;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class FeedbackmanagementApplication {
+@ComponentScan("com.hackfse")
+@ImportResource("classpath:META-INF/persistence.xml")
+public class FeedbackmanagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbackmanagementApplication.class, args);
 	}
-
+	
+	/*@Bean
+	public LocalEntityManagerFactoryBean entityManagerFactory(){
+	     LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+	    factoryBean.setPersistenceUnitName("sampleManager");
+	    return factoryBean;
+	}*/
 }
 

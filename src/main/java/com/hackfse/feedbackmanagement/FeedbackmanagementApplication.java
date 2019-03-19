f22d7edbffafb3ef4b877372ab6383cf2fa7f11b
AntonyJohn <m.antonyjohn@gmail.com>
2019-03-20 00:07:13 +0530

    test

--- src/main/java/com/hackfse/feedbackmanagement/FeedbackmanagementApplication.java
+++ src/main/java/com/hackfse/feedbackmanagement/FeedbackmanagementApplication.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class FeedbackmanagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		System.out.println("TEst123 >>>>>>>>");
+		System.out.println("TEst12 >>>>>>>>");
 		SpringApplication.run(FeedbackmanagementApplication.class, args);
 	}
 	

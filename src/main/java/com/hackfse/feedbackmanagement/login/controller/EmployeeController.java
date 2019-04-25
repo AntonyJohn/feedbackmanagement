16e88d01f1cb67d7ab0a24adb0b78d73f55bf7c3
Antonyjohn <m.antonyjohn@gmail.com>
2019-04-25 09:14:47 +0530

    test

--- src/main/java/com/hackfse/feedbackmanagement/login/controller/EmployeeController.java
+++ src/main/java/com/hackfse/feedbackmanagement/login/controller/EmployeeController.java
@@ -40,7 +40,7 @@ public class EmployeeController {
     @RequestMapping(value="/retrieveEmployee", method=RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public@ResponseBody Employee  retrieveEmployee(@RequestParam("empID") String empID){					
-    	LOG.info("Start:: EmployeeController --> retrieveEmployee()");    	
+    	LOG.info("Start:: EmployeeController123 --> retrieveEmployee()");    	
         emp = new Employee();       
         emp.setId(new Integer(empID));
         Employee obj=employeeService.retrieveEmployee(emp,emp.getId());
